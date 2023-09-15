07a9cccc05e9d827fc28200f8d984781f133b404
Azariah <m04sengy@gmail.com>
2023-09-15 11:29:00 +0200

    Add code for angular-16.2.0 create-workspace.
    [Url: razroo.com/community/angular-16.2.0/create-workspace-recipe/create-workspace]

---
+++ apps/azaeng04/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'azaeng04-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'azaeng04';
+}
